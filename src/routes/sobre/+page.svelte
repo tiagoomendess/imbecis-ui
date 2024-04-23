01a5faef89e91eecf81190755ac8472a271ddcbd
Tiago Mendes <tiagoomendess@gmail.com>
2024-04-24 00:01:51 +0100

    better about page

--- src/routes/sobre/+page.svelte
+++ src/routes/sobre/+page.svelte
@@ -1,10 +1,10 @@
 <script>
-	import { Heading, P, A, Img, List, Li } from 'flowbite-svelte';
+	import { Heading, P, A, Img, List, Li, Button } from 'flowbite-svelte';
 </script>
 
 <svelte:head>
 	<title>Imbecis :: Sobre</title>
-	<meta property="og:image" content="/imbecis_logo_300.png">
+	<meta property="og:image" content="/imbecis_logo_300.png" />
 	<meta
 		property="og:description"
 		content="Esta app está a ser desenvolvida que está por alguém que está farto de ver pessoas egoístas
@@ -23,14 +23,18 @@
 </div>
 
 <P class="mb-3 text-justify">
-	Esta webapp é um protesto ao estado das nossas ruas e à ineficácia das autiridades. <b>Está a ser desenvolvida</b>
-	(beta v0.8) por alguém que está farto de ver pessoas egoístas a fazer o que lhes apetece na rua, sem qualquer respeito pelo próximo,
-	violando constantemente o código da estrada e os deveres cívicos mais básicos sem nenhuma consequência. Esta é uma consequência, e uma forma de
-	tentar educar o condutor Português.
+	Esta webapp é um protesto ao estado das nossas ruas e à ineficácia das autiridades. <b
+		>Está a ser desenvolvida</b
+	>
+	(beta v0.8) por alguém que está farto de ver pessoas egoístas a fazer o que lhes apetece na rua, sem
+	qualquer respeito pelo próximo, violando constantemente o código da estrada e os deveres cívicos mais
+	básicos sem nenhuma consequência. Esta é uma consequência, e uma forma de tentar educar o condutor
+	Português.
 </P>
 <P class="mb-7 text-justify">
-	Explicando o funcionamento da aplicação de forma muito resumida, utilizadores não autenticados na internet submetem fotografias de veículos
-	que acham estar estacionados de forma abusiva, e outros utilizadores também não autenticados votam se acham que é ou não é um imbecil.
+	Explicando o funcionamento da aplicação de forma muito resumida, utilizadores não autenticados na
+	internet submetem fotografias de veículos que acham estar estacionados de forma abusiva, e outros
+	utilizadores também não autenticados votam se acham que é ou não é um imbecil.
 </P>
 
 <Heading tag="h3" class="mb-2">Índice</Heading>
@@ -38,6 +42,7 @@
 <List class="pl-3 mb-7">
 	<Li><A href="#permissoes">Permissões e Privacidade</A></Li>
 	<Li><A href="#como_participar">Como Participar?</A></Li>
+	<Li><A href="#ajudar">Ajudar</A></Li>
 	<Li><A href="#motivacao">Motivação</A></Li>
 	<Li><A href="#policia">A Polícia não faz nada</A></Li>
 	<Li><A href="#remover">Pedidos de Remoção</A></Li>
@@ -72,11 +77,12 @@
 	Para os imbecís egoístas ainda é mais facil, basta não ser um peso morto na sociedade e não violar
 	o código da estrada, não estacionando de forma abusiva. É garantido que a sua viatura não aparece
 	na app. De qualquer das formas a matricula é desfocada antes de ser pública, e é impossível saber
-	qual era a que lá estava porque não são guardadas copias nem o original. Por isso <b>a matrícula que
-	aparece à frente em texto pode não ser a que está na imagem. Não é possível saber nem garantir que
-	que é a mesma</b>, uma vez que o conteúdo é um campo de texto livre que é preenchido manualmente por utilizadores
-	anónimos da internet, que <b>podem inserir o que quiserem</b>, como um comentário numa rede social. E a imagem
-	foi irreversivelmente desfocada antes de ser publicada como no Google Maps.
+	qual era a que lá estava porque não são guardadas copias nem o original. Por isso <b
+		>a matrícula que aparece à frente em texto pode não ser a que está na imagem. Não é possível
+		saber nem garantir que que é a mesma</b
+	>, uma vez que o conteúdo é um campo de texto livre que é preenchido manualmente por utilizadores
+	anónimos da internet, que <b>podem inserir o que quiserem</b>, como um comentário numa rede
+	social. E a imagem foi irreversivelmente desfocada antes de ser publicada como no Google Maps.
 </P>
 
 <Heading id="como_participar" tag="h3" class="mb-2">Como Participar?</Heading>
@@ -90,25 +96,35 @@
 <P class="mb-3 text-justify">
 	Os imbecis aprovados pelos outros utilizadores aparecem na <A href="/">pagina inicial</A>. Todas
 	as matrículas de imbecis confirmados aparecem
-	<A href="/matriculas">na lista de matrículas</A>. Alternativamente pode pesquisar por uma matrícula <A
-		href="/procurar">nesta página</A
-	>.
+	<A href="/matriculas">na lista de matrículas</A>. Alternativamente pode pesquisar por uma
+	matrícula <A href="/procurar">nesta página</A>.
 </P>
 <P class="mb-3 text-justify">
-	Pode também votar nos potenciais imbecis que foram enviados por outros utilizadores. Apenas confirme
-	aqueles que de facto tem a certeza que está em incumprimento e que consegue ler claramente a matrícula.
-	Em todos os outros casos, com mais ou menos dúvida, clicar em "Não Tenho a Certeza".
-</P>
-<P class="mb-7 text-justify">
-	Se quiser fazer ainda mais por esta causa, pode ainda fazer download do <A
-		href="/imbecis_cartaz.pdf"
-		target="_blank">cartaz A4</A
-	>
-	para imprimir e colocar em zonas habitualmente infestadas por imbecis. Ou fazer o download deste
-	<A href="/imbecis_registado.pdf" target="_blank">panfleto</A> para colocar nos parabrisas dos imbecis
-	que encontrar depois de adicionar à App.
+	Pode também votar nos potenciais imbecis que foram enviados por outros utilizadores. Apenas
+	confirme aqueles que de facto tem a certeza que está em incumprimento e que consegue ler
+	claramente a matrícula. Em todos os outros casos, com mais ou menos dúvida, clicar em "Não Tenho a
+	Certeza".
 </P>
 
+<Heading id="ajudar" tag="h3" class="mb-2">Ajudar</Heading>
+
+<P class="mb-3 text-justify">
+	Se quiser fazer ainda mais por esta causa para além de enviar e classificar imbecis, pode começar por
+	partilhar esta aplicação com os seus amigos e familiares, para que chegue e sensibilize o maior
+	número de pessoas possível. Pode ainda fazer download de um cartaz para imprimir e
+	colocar em zonas que habitualmente sofrem com o estacionamento abusivo, para chegar também a essas pessoas.
+</P>
+<div class="flex justify-center mb-3">
+	<Button href="/imbecis_cartaz.pdf" target="_blank">Descarregar Cartaz</Button>
+</div>
+<P class="mb-3 text-justify">
+	Pode tambem fazer download de um panfleto para colocar nos parabrisas de carros estacionados de forma abusiva
+	depois de os adicionar à aplicação.
+</P>
+
+<div class="flex justify-center mb-7">
+	<Button href="/imbecis_registado.pdf" target="_blank">Descarregar Panfleto</Button>
+</div>
 
 <Heading id="motivacao" tag="h3" class="mb-2">Motivação</Heading>
 
@@ -137,10 +153,11 @@
 ></iframe>
 
 <P class="mb-3 text-justify">
-	Caminhamos no sentido oposto do progresso, quando as principais economias da europa procuram reduzir a dependência do automóvel,
-	nós incentivamos o uso e abuso dele. O que se passa em Portugal não acontece nos países mais
-	desenvolvidos e ricos que o nosso, por alguma razão. Existem consequências, e nos meses de verão podemos observar Portugueses
-	Emigrantes a fazer cá o que com certeza não fazem lá.
+	Caminhamos no sentido oposto do progresso, quando as principais economias da europa procuram
+	reduzir a dependência do automóvel, nós incentivamos o uso e abuso dele. O que se passa em
+	Portugal não acontece nos países mais desenvolvidos e ricos que o nosso, por alguma razão. Existem
+	consequências, e nos meses de verão podemos observar Portugueses Emigrantes a fazer cá o que com
+	certeza não fazem lá.
 </P>
 
 <P class="mb-5 text-justify">
@@ -153,17 +170,17 @@
 <Heading id="policia" tag="h3" class="mb-2">A polícia não faz nada</Heading>
 
 <P class="mb-3 text-justify">
-	Esta não é uma ferramenta de denúncia à polícia, pelo menos por enquanto. Para isso existem estas duas aplicações que
-	recomendo: "Aqui Não" e "DenúnciaEstacionamento". No entanto está a ser desenvolvida esta funcionalidade e será lançada no
-	futuro.
+	Esta não é uma ferramenta de denúncia à polícia, pelo menos por enquanto. Para isso existem estas
+	duas aplicações que recomendo: "Aqui Não" e "DenúnciaEstacionamento". No entanto está a ser
+	desenvolvida esta funcionalidade e será lançada no futuro.
 </P>
 
 <P class="mb-3 text-justify">
 	Esta aplicação surge precisamente pela inação e/ou ineficácia da polícia em certos pontos do país.
 	Não acontece em 100% do território, mas em muitas localidades as autoridades fazem vista grossa e
-	ignoram os abusos, perpetuando a impunidade, a falta de respeito e incentivando a continuação do ato ilícito.
-	Em variados casos as denúncias enviadas através dos meios legais são ignoradas, não resultando em nenhuma consequência
-	para o infrator.
+	ignoram os abusos, perpetuando a impunidade, a falta de respeito e incentivando a continuação do
+	ato ilícito. Em variados casos as denúncias enviadas através dos meios legais são ignoradas, não
+	resultando em nenhuma consequência para o infrator.
 </P>
 
 <P class="mb-7 text-justify">
