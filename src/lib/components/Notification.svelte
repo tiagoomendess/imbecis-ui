f6a530b8efc528723c09552080f9a08890f5fcd4
Tiago Mendes <tiagoomendess@gmail.com>
2026-04-28 22:24:40 +0100

    fix 2

--- src/lib/components/Notification.svelte
+++ src/lib/components/Notification.svelte
@@ -1,7 +1,7 @@
 <script lang="ts">
 	import type { NotificationType } from '../types';
 	import { Toast } from 'flowbite-svelte';
-	import { CheckCircleSolid, CloseSolid, ExclamationCircleSolid } from 'flowbite-svelte-icons';
+	import { CheckCircleSolid, CloseCircleOutline, ExclamationCircleSolid } from 'flowbite-svelte-icons';
 	import { onMount } from 'svelte';
 	import { fly } from 'svelte/transition';
 	import * as Icon from 'flowbite-svelte-icons';
@@ -36,7 +36,7 @@
 	{#if type === 'error'}
 		<Toast color="red" transition={fly} params={{ y: -200 }} bind:open>
 			<svelte:fragment slot="icon">
-				<CloseSolid class="w-5 h-5" />
+				<CloseCircleOutline class="w-5 h-5" />
 				<span class="sr-only">Error icon</span>
 			</svelte:fragment>
 			{message}
